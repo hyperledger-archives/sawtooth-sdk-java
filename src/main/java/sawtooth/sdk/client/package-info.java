04515a0e318334e8496cc0cedbef5f5852d5ba1b
Boyd Johnson <bjohnson@bitwise.io>
2018-10-10 09:55:33 -0500

    Fix sun checkstyle lint in sawtooth.sdk.client package
    Signed-off-by: Boyd Johnson <bjohnson@bitwise.io>

---
+++ src/main/java/sawtooth/sdk/client/package-info.java
@@ -0,0 +1,6 @@
+/** The client package has a Signing class that allows for producing hex encoded signatures
+ *  that are needed in the BatchHeader and TransactionHeader.
+ *
+ */
+package sawtooth.sdk.client;
+
