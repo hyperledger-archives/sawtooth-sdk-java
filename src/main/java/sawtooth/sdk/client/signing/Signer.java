18414e167b55858980bb6dce6f9efbd754081271
Boyd Johnson <bjohnson@bitwise.io>
2018-10-19 09:32:47 -0500

    Add convenience Signer class
    This class wraps the Context and PrivateKey implementations.
    
    Signed-off-by: Boyd Johnson <bjohnson@bitwise.io>

---
+++ src/main/java/sawtooth/sdk/client/signing/Signer.java
@@ -0,0 +1,60 @@
+/* Copyright 2018 Bitwise IO, Inc.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+------------------------------------------------------------------------------*/
+
+package sawtooth.sdk.client.signing;
+
+/**
+ * Convenience class that wraps the PrivateKey and the Context.
+ */
+public class Signer {
+
+  /**
+   * The `sawtooth.sdk.client.signing.Context` implementation.
+   */
+  private Context mContext;
+  /**
+   * The `sawtooth.sdk.client.signing.PrivateKey` implementation.
+   */
+  private PrivateKey mPrivateKey;
+
+  /**
+   * Constructor.
+   *
+   * @param aContext Context
+   * @param aPrivateKey PrivateKey
+   */
+  public Signer(final Context aContext, final PrivateKey aPrivateKey) {
+    this.mContext = aContext;
+    this.mPrivateKey = aPrivateKey;
+  }
+
+  /**
+   * Produce a Hex encoded signature from the data and the PrivateKey.
+   *
+   * @param data byte[]
+   * @return String signature
+   */
+  public final String sign(final byte[] data) {
+    return this.mContext.sign(data, this.mPrivateKey);
+  }
+
+  /**
+   * Get the public key associated with the private key.
+   *
+   * @return PublicKey
+   */
+  public final PublicKey getPublicKey() {
+    return this.mContext.getPublicKey(this.mPrivateKey);
+  }
+}
