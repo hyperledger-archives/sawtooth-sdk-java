13f0b59c2ba2fc5dc4364944c5d4ecf1998638f4
Boyd Johnson <bjohnson@bitwise.io>
2018-11-05 09:59:32 -0600

    Reorganize java sdk into three modules
    The modules are
    	- sawtooth-sdk-protos
    	- sawtooth-sdk-signing
    	- sawtooth-sdk-transaction-processor
    
    Signed-off-by: Boyd Johnson <bjohnson@bitwise.io>

--- src/main/java/sawtooth/sdk/client/package-info.java
+++
@@ -1,6 +0,0 @@
-/** The client package has a Signing class that allows for producing hex encoded signatures
- *  that are needed in the BatchHeader and TransactionHeader.
- *
- */
-package sawtooth.sdk.client;
-
