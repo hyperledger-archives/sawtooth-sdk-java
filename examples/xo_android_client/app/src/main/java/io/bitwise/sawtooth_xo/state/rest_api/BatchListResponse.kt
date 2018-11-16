4dbaefb53a4f1b25a6147228f428a46cd72adc4c
Eloá França Verona <eloafran@bitwise.io>
2018-11-16 10:07:43 -0600

    Add service to communicate with sawtooth-rest-api
    This commit adds a webservice to communicate with the
    res-api and models to deserialize the json response
    received from the rest-api
    
    Signed-off-by: Eloá França Verona <eloafran@bitwise.io>
    Co-Authored-By: Boyd Johnson <bjohnson@bitwise.io>

---
+++ examples/xo_android_client/app/src/main/java/io/bitwise/sawtooth_xo/state/rest_api/BatchListResponse.kt
@@ -0,0 +1,20 @@
+package io.bitwise.sawtooth_xo.state.rest_api
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Data class for interacting with the Sawtooth REST Api
+ */
+class BatchListResponse(
+    @field:SerializedName("link")
+    /** Returns the link to query the batch status
+     *
+     * @return String link
+     */
+    val link: String
+
+) {
+    override fun toString(): String {
+        return "BatchListResponse(link='$link')"
+    }
+}
---
+++ examples/xo_android_client/app/src/main/java/io/bitwise/sawtooth_xo/state/rest_api/SawtoothRestApi.kt
@@ -0,0 +1,17 @@
+package io.bitwise.sawtooth_xo.state.rest_api
+
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface SawtoothRestApi {
+
+    @POST("/batches")
+    fun postBatchList(@Body payload: RequestBody): Call<BatchListResponse>
+
+    @GET("/batch_statuses")
+    fun getBatchStatus(@Query("id") batch_id: String, @Query("wait") wait: Int): Call<BatchStatusResponse>
+}
