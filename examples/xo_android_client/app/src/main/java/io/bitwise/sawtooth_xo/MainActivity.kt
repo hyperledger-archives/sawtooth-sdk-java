608608a8660b016c79d67933a2ecf93e17ccd7da
Eloá França Verona <eloafran@bitwise.io>
2018-11-19 11:53:01 -0600

    Add floating action button to list board view
    In the future this button will be used to access the activity in which
    the user can create a new game
    
    Signed-off-by: Eloá França Verona <eloafran@bitwise.io>

--- examples/xo_android_client/app/src/main/java/io/bitwise/sawtooth_xo/MainActivity.kt
+++ examples/xo_android_client/app/src/main/java/io/bitwise/sawtooth_xo/MainActivity.kt
@@ -2,16 +2,11 @@ package io.bitwise.sawtooth_xo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import io.bitwise.sawtooth_xo.state.XoState
-import java.util.UUID
-
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val state =  XoState()
-        state.createGame("game-" + UUID.randomUUID().toString())
         setContentView(R.layout.activity_main)
     }
 }
