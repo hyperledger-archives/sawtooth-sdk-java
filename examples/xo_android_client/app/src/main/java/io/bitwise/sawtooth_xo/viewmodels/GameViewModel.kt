42850e001935ceff64fcbd95096be89f7ab8a8bf
Eloá França Verona <eloafran@bitwise.io>
2018-11-27 09:30:01 -0600

    Add GameViewModel
    Adds class that will be responsible to serve the Game
    data to activities and fragments.
    
    Signed-off-by: Eloá França Verona <eloafran@bitwise.io>

---
+++ examples/xo_android_client/app/src/main/java/io/bitwise/sawtooth_xo/viewmodels/GameViewModel.kt
@@ -0,0 +1,26 @@
+package io.bitwise.sawtooth_xo.viewmodels
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Transformations
+import android.arch.lifecycle.ViewModel
+import io.bitwise.sawtooth_xo.models.Game
+import io.bitwise.sawtooth_xo.state.XoState
+
+class GameViewModel : ViewModel(){
+    private val state: XoState = XoState()
+    var games: LiveData<List<Game>> = Transformations.switchMap(state.games) {input ->
+        var m = MutableLiveData<List<Game>>()
+        m.value = input
+        m
+    }
+    
+    init {
+        state.getState(true)
+    }
+
+    fun loadGames(update: Boolean) {
+      state.getState(update)
+    }
+}
+
